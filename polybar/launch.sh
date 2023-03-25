d803dbc8decb19c7eadeb83e57196f2299fe8e29
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2023-03-24 23:40:26 -0400

    more

---
+++ polybar/launch.sh
@@ -0,0 +1,18 @@
+killall -q polybar
+
+#if type "xrandr"; then
+#	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#		MONITOR=$m polybar --reload main &
+#	done
+#else
+#	polybar --reload main &
+#fi
+
+
+# [ -n "`xrandr -q | grep 'DP-0 connected'`" ] && MONITOR=DP-0 TRAY_POS=right polybar --reload main &
+# [ -n "`xrandr -q | grep 'DP-1-1 connected'`" ] && MONITOR=DP-1-1 polybar --reload second &
+
+polybar --reload main &
+polybar --reload second &
+
+
