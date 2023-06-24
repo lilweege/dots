b48b9e8e0605b4a20a6c720537866736c1229266
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2023-06-24 13:33:48 -0400

    upgrade bar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
-killall -q polybar
+#killall -q polybar
 
 #if type "xrandr"; then
 #	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
@@ -12,7 +12,5 @@ killall -q polybar
 # [ -n "`xrandr -q | grep 'DP-0 connected'`" ] && MONITOR=DP-0 TRAY_POS=right polybar --reload main &
 # [ -n "`xrandr -q | grep 'DP-1-1 connected'`" ] && MONITOR=DP-1-1 polybar --reload second &
 
-polybar --reload main &
-polybar --reload second &
-
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
